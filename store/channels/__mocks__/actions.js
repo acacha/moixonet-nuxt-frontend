4f60e93e112c43db4340a31eca2673ec7dde6c48
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-11-21 17:13:43 +0100

    Sync

--- store/channels/__mocks__/actions.js
+++
@@ -1,6 +0,0 @@
-// import * as actionTypes from '../../action-types'
-//
-// export default {
-//   [actionTypes.CHANNEL_STORE]: jest.fn().mockResolvedValue(),
-//   [actionTypes.CHANNEL_DESTROY]: jest.fn().mockResolvedValue()
-// }
