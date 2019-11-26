b3d34941340f3d31ee7ffb9f0c39ef3fa2d39be7
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-11-26 21:27:14 +0100

    Funcions amb Javascript i PHP

--- components/ChannelsForm.vue
+++ components/ChannelsForm.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>FORM HERE</h1>
+  <span>
+    <v-btn @click="show = !show" small fab dark color="accent">
+      <v-icon dark>mdi-plus</v-icon>
+    </v-btn>
+    <v-dialog v-model="show" width="500px">
+      <v-card>
+        <v-toolbar dense color="primary">
+          <v-toolbar-title class="white--text">Afegeix un canal</v-toolbar-title>
+        </v-toolbar>
+        <v-container>
+          <v-form>
+            <v-text-field name="name" label="Nom" placeholder="Nom" />
+          </v-form>
+        </v-container>
+        <v-card-actions>
+          <v-btn @click="show = !show">Tancar</v-btn>
+          <v-btn color="primary">Afegir</v-btn>
+        </v-card-actions>
+      </v-card>
+
+    </v-dialog>
+  </span>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  }
+}
+</script>
